ac06b84ca7ba6df1a8419acfa05fa438de9e0739
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-12 15:25:55 +0530

    Update testCc.js

--- chaincode/newcc/testCc.js
+++ chaincode/newcc/testCc.js
@@ -36,7 +36,7 @@ let channelID= await stub.getChannelID();
 let parms=await stub.getArgs();
 let txId= await stub.getTxID();
 let timeStamp= await stub.getTxTimestamp();
-let time = Buffer.from(timeStamp);
+return Buffer.from(timeStamp);
 console.log('submitter is..'+user);
 console.log('channel Id..'+channelID);
 console.log('function args..'+parms);
