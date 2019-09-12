e1658fdcbcf76cff654b514e3beaf34d87431991
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-12 15:31:15 +0530

    Update testCc.js

--- chaincode/newcc/testCc.js
+++ chaincode/newcc/testCc.js
@@ -36,12 +36,12 @@ let channelID= await stub.getChannelID();
 let parms=await stub.getArgs();
 let txId= await stub.getTxID();
 let timeStamp= await stub.getTxTimestamp();
-return Buffer.from(timeStamp);
+// return Buffer.from(timeStamp);
 console.log('submitter is..'+user);
 console.log('channel Id..'+channelID);
 console.log('function args..'+parms);
 console.log('transaction Id is..'+txId);
-console.log('timestamp is..'+time);
+//console.log('timestamp is..'+time);
 
     }
 }
