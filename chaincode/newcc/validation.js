67709d7d85cf39b74d63d0c1db63564e65e45211
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-17 20:56:19 +0530

    Update validation.js

--- chaincode/newcc/validation.js
+++ chaincode/newcc/validation.js
@@ -53,18 +53,20 @@ async login(stub, args) {
     let userName=args[0];
     let password=args[1];
     let credentialsAsBytes = await stub.getState(args[0]); 
-    let credentials= JSON.parse(credentialsAsBytes);
-
+    
 if (!credentialsAsBytes || credentialsAsBytes.toString().length <= 0) {
     throw new Error('Incorrect Username..!');
          }
-
+else{
+let credentials= JSON.parse(credentialsAsBytes);
 if (password!=credentials.password) {
     throw new Error('Incorrect Password..!');
         }
+
 // Functions go here after signin
 console.log('Login Successfull..✓')
       }
+   }
 
 }
 
