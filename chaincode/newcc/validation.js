f5d3da680b4a681d09f9ef6f1720d7dfe790d3cd
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-17 18:37:48 +0530

    Update validation.js

--- chaincode/newcc/validation.js
+++ chaincode/newcc/validation.js
@@ -3,13 +3,12 @@ const shim = require('fabric-shim');
 const util = require('util');
 let Chaincode = class {
  
-    async Init(stub) {
+async Init(stub) {
     console.info('=========== Instantiated test chaincode ===========');
     return shim.success();
-      }
+        }
 
-  
-  async Invoke(stub) {
+async Invoke(stub) {
     let ret = stub.getFunctionAndParameters();
     console.info(ret);
 
@@ -17,28 +16,26 @@ let Chaincode = class {
     if (!method) {
       console.error('no function of name:' + ret.fcn + ' found');
       throw new Error('Received unknown function ' + ret.fcn + ' invocation');
-    }
+         }
     try {
       let payload = await method(stub, ret.params);
       return shim.success(payload);
-         } catch (err) {
+         } 
+     catch (err) {
       console.log(err);
       return shim.error(err);
-         }
+            }
      }
   
 async signUp(stub, args) {
       if (args.length != 2) {
       throw new Error('Incorrect number of arguments. Expecting 2');
-          }
+            }
 let credentialsAsBytes = await stub.getState(args[0]); 
 if (!credentialsAsBytes || credentialsAsBytes.toString().length <= 0) {
-  console.info('**Storing Credentials on Blockchain**');
+    console.info('**Storing Credentials on Blockchain**');
 
-        const credentials  = {
-            userName:args[0],
-            password:args[1]  
-          };
+    const credentials  = {userName:args[0],password:args[1]};
 
     await stub.putState(args[0], Buffer.from(JSON.stringify(credentials)));
     console.info('*Signup Successfull..Your Username is  '+args[0]);
@@ -57,7 +54,7 @@ async login(stub, args) {
     let userName=args[0];
     let password=args[1];
     let credentialsAsBytes = await stub.getState(args[0]); 
-    let credentials=JSON.parse(credentialsAsBytes);
+    let credentials=JSON.parse(credentialsAsBytes.toString());
     if (userName!=credentials.userName) {
       throw new Error('Incorrect Username..!');
            }
